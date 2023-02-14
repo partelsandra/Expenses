27d5984877e596f9daed88c18c37365fad0f59b9
Sandra Partel <sandra.partel.001@voco.ee>
2023-02-14 14:23:23 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-import Expenses from "./components/Expenses";
-import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
-  const expenses = [
-    {
-      date: new Date(2023, 0, 10),
-      title:'New book',
-      price: 30.99
-    },
-    {
-      date: new Date(2023, 0, 5),
-      title:'Icecream',
-      price: 3.99
-    }
-  ]
+import './App.css';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import {useState} from "react";
+
+
+
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    date: new Date(2024, 0, 10),
+    title:'New book',
+    amount: 30.99
+  },
+  {
+    id: 'e2',
+    date: new Date(2024, 0, 5),
+    title:'Icecream',
+    amount: 3.99
+  },
+  {
+    id: 'e3',
+    date: new Date(2023, 0, 26),
+    title:'Bag',
+    amount: 399.99
+  }
+
+]
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+  const [filteredYear, setFilteredYear] = useState('2023')
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year)
+  }
+
+
+  const addExpensehandler = (expense) => {
+    console.log('In App.js')
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses]
+    })
+  }
+  console.log(expenses)
 
   return (
       <div className="App">
-        <Expenses expenseData={expenses}></Expenses>
+        <NewExpense onAddExpense={addExpensehandler}></NewExpense>
+        <Expenses expenses={expenses}></Expenses>
+
       </div>
   );
 }
+
+
+
 
 export default App;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,10 @@
 import './Card.css'
 
-function Card (props) {
-    const classes = 'card' + props.className
-    return (
+const Card = (props) =>{
+    const classes = 'card ' + props.className
+    return(
         <div className={classes}>{props.children}</div>
     )
 }
+
 export default Card
